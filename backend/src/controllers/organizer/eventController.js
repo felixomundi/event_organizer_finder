f73184dedf37035b61c1336f900594943b8f5ddf
Felix Omundi <91674030+felixomundi@users.noreply.github.com>
2024-01-25 09:38:37 +0300

    first commit

--- backend/src/controllers/organizer/eventController.js
+++ backend/src/controllers/organizer/eventController.js
@@ -66,10 +66,88 @@ async function deleteEventByOrganizer(req, res) {
         });    
     }
 }
+async function updateEventStatusByOrganizer(req, res) { 
+    try {
+        const { event_id } = req.body;
+        if (!event_id) {
+            return res.status(400).json({
+                message:"An event id is required"
+            });    
+        }
+        const event_exists = await Event.findOne({
+            where:{id:event_id, userId:req.user.id}
+        })
+        if (!event_exists) {
+            return res.status(400).json({
+                message:"An event not Found"
+            });    
+        }
+        if (event_exists.status == "active") {
+            await event_exists.update({
+                status:"inactive"
+            });
+            return res.status(200).json({
+                message:"Event Status DeActivated Successfully"
+            });  
+        } else {
+            await event_exists.update({
+                status:"active"
+            });
+            return res.status(200).json({
+                message:"Event Status Activated Successfully"
+            });  
+        }        
+    } catch (error) {
+        return res.status(500).json({
+            message:"Error in Updating Event"
+        });  
+    }
+}
+async function updateEventByOrganizer(req, res){
+    try {
+       
+        const event_id = req.body.event_id;
+        if (!event_id) {
+            return res.status(400).json({
+                message:"An event id is required"
+            });    
+        }
+        const event = await Event.findOne({
+            where:{id:event_id, userId:req.user.id}
+        })
+        if (!event) {
+            return res.status(400).json({
+                message:"Event not found"
+            })
+        }
+        const { id, event_name, location,time_start, time_end,status, event_date,details, no_of_participants, entry_fee } = event;       
+        event.id = id
+        event.event_name = event_name || req.body.event_name
+        event.location =  req.body.location || location
+        event.time_start = req.body.time_start || time_start
+        event.time_end =  req.body.time_end || time_end
+        event.event_date =  await generateDate(new Date(req.body.event_date)) ||await generateDate(new Date(event_date));
+        event.details = req.body.details || details
+        event.no_of_participants =  req.body.no_of_participants || no_of_participants
+        event.entry_fee = req.body.entry_fee || entry_fee
+        event.status = req.body.status || status;
+        const updatedEvent = await event.update();
+        return res.status(200).json({
+            event:updatedEvent            
+        })
 
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Error in Updating Event"
+        }); 
+    }
+}
 
 module.exports = {
     getOrganizerEvents,
     createEventByOrganizer,
-    deleteEventByOrganizer
+    deleteEventByOrganizer,
+    updateEventStatusByOrganizer,
+    updateEventByOrganizer
 }
--- backend/src/routes/eventRoute.js
+++ backend/src/routes/eventRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getOrganizerEvents, createEventByOrganizer, deleteEventByOrganizer } = require("../controllers/organizer/eventController");
+const { getOrganizerEvents, createEventByOrganizer, deleteEventByOrganizer, updateEventStatusByOrganizer, updateEventByOrganizer } = require("../controllers/organizer/eventController");
 const  isOrganizer  = require("./../middleware/organizerMiddleware");
 const router = express.Router();
 router.get("/", isOrganizer, getOrganizerEvents);
 router.post("/", isOrganizer, createEventByOrganizer);
+router.post("/update", isOrganizer, updateEventStatusByOrganizer);
+router.put("/update_event", isOrganizer, updateEventByOrganizer);
 router.get("/:id", isOrganizer, deleteEventByOrganizer);
 module.exports = router;
